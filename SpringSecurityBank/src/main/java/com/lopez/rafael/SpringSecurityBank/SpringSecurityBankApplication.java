c76ab309c601b348382242c0695523f22fc2a001
Rafael Lopez <rafael.lopez@bbd.ca>
2021-03-13 16:57:43 -0500

    Spring Security Bank project

---
+++ SpringSecurityBank/src/main/java/com/lopez/rafael/SpringSecurityBank/SpringSecurityBankApplication.java
@@ -0,0 +1,13 @@
+package com.lopez.rafael.SpringSecurityBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBankApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityBank/src/test/java/com/lopez/rafael/SpringSecurityBank/SpringSecurityBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopez.rafael.SpringSecurityBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
