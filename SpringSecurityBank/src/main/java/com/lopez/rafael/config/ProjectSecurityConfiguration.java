23283940de71bbf2e5147f50d9dadd79ae21e4ff
Rafael Lopez <rafael.lopez@bbd.ca>
2021-03-13 18:43:59 -0500

    Configure users using inMemoryUserDetailsManager

--- SpringSecurityBank/src/main/java/com/lopez/rafael/config/ProjectSecurityConfiguration.java
+++ SpringSecurityBank/src/main/java/com/lopez/rafael/config/ProjectSecurityConfiguration.java
@@ -1,11 +1,16 @@
 package com.lopez.rafael.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -52,15 +57,34 @@ public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
 //        http.authorizeRequests().anyRequest().permitAll().and().formLogin().and().httpBasic();
     }
 
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//            .inMemoryAuthentication()
+//                .withUser("admin").password("admin").authorities("admin")
+//                .and()
+//                .withUser("user").password("user").authorities("read")
+//                .and()
+//                //Always pass it. If no passwordEncoder, Spring doesn't know how to process password -> throws error
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
+
+    /*
+     * Another way to do the same as above. This one is clearer, but requires a PasswordEncoder bean
+     */
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("admin").password("admin").authorities("admin")
-                .and()
-                .withUser("user").password("user").authorities("read")
-                .and()
-                //Always pass it. If no passwordEncoder, Spring doesn't know how to process password -> throws error
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails admin = User.withUsername("admin").password("admin").authorities("admin").build();
+        UserDetails user = User.withUsername("user").password("user").authorities("read").build();
+        userDetailsService.createUser(admin);
+        userDetailsService.createUser(user);
+
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
