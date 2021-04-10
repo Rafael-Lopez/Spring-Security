5f796489519e42d75d70f4be7c7962a5a05290e5
Rafael Lopez <rafael.lopez@bbd.ca>
2021-04-09 21:25:57 -0400

    Add CORS configuration to resolve CORS issue

--- SpringSecurityBank/src/main/java/com/lopez/rafael/config/ProjectSecurityConfiguration.java
+++ SpringSecurityBank/src/main/java/com/lopez/rafael/config/ProjectSecurityConfiguration.java
@@ -14,8 +14,12 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -39,6 +43,21 @@ public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
          * Custom configuration as per our requirements
          */
         http
+            .cors()
+                .configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+
+                        return config;
+                    }
+                })
+            .and()
             .authorizeRequests()
                 .antMatchers("/account").authenticated()
                 .antMatchers("/balance").authenticated()
