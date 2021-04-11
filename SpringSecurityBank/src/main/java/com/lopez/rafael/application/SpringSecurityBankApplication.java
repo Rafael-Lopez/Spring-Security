44383ddf8482daa2f64b8fb5fc684bea3980437b
Rafael Lopez <rafael.lopez@bbd.ca>
2021-04-11 14:15:31 -0400

    Debug built-in Spring Security filters

--- SpringSecurityBank/src/main/java/com/lopez/rafael/application/SpringSecurityBankApplication.java
+++ SpringSecurityBank/src/main/java/com/lopez/rafael/application/SpringSecurityBankApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.lopez.rafael.repository")
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 		@ComponentScan("com.lopez.rafael.controller"),
 		@ComponentScan("com.lopez.rafael.config")
 })
+@EnableWebSecurity(debug = true)
 public class SpringSecurityBankApplication {
 
 	public static void main(String[] args) {
