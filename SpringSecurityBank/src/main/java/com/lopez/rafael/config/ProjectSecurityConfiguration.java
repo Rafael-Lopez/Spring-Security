1d50d667d10f84c256ca5ffcf4df5489473abcfa
Rafael Lopez <rafael.lopez@bbd.ca>
2021-04-13 13:39:21 -0400

    Implement method level security using @PreAuthorize (and @PostAuthorize)

--- SpringSecurityBank/src/main/java/com/lopez/rafael/config/ProjectSecurityConfiguration.java
+++ SpringSecurityBank/src/main/java/com/lopez/rafael/config/ProjectSecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.lopez.rafael.filter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
@@ -28,6 +29,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     /*
@@ -91,7 +93,7 @@ public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 //we have to add roles with the "ROLE_" prefix, but we don't need it in the code.
                 .antMatchers("/myAccount").hasRole("USER")
                 .antMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/myLoans").hasRole("ROOT")
+                .antMatchers("/myLoans").authenticated()
                 //Any user that is authenticated
                 .antMatchers("/myCards").authenticated()
                 .antMatchers("/user").authenticated()
--- SpringSecurityBank/src/main/java/com/lopez/rafael/repository/LoanRepository.java
+++ SpringSecurityBank/src/main/java/com/lopez/rafael/repository/LoanRepository.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import com.lopez.rafael.model.Loans;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoanRepository extends CrudRepository<Loans, Long> {
-	
+	//No user has ROOT role, so the method should not be invoked
+	//@PreAuthorize("hasRole('ROOT')")
+	@PreAuthorize("hasRole('USER')")
 	List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
 
 }
